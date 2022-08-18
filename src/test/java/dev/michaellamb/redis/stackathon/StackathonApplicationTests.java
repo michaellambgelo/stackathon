a013eaac3177db02ccb9918f27d5e01d1016f3d0
Michael Lamb <michaellambgelo@users.noreply.github.com>
2022-08-17 20:50:34 -0500

    initial commit

---
+++ src/test/java/dev/michaellamb/redis/stackathon/StackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.michaellamb.redis.stackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
