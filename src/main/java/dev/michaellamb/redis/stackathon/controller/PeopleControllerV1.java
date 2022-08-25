4469131638057b4fbc8f4b57e832b47cf07b6920
Michael Lamb <michaellambgelo@users.noreply.github.com>
2022-08-24 20:25:54 -0500

    fix build

--- src/main/java/dev/michaellamb/redis/stackathon/controller/PeopleControllerV1.java
+++ src/main/java/dev/michaellamb/redis/stackathon/controller/PeopleControllerV1.java
@@ -1,12 +1,8 @@
 package dev.michaellamb.redis.stackathon.controller;
 
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.Metrics;
-import org.springframework.data.geo.Point;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,30 +28,10 @@ public class PeopleControllerV1 {
     return peopleRepository.findAll();
   }
 
-  @GetMapping("age_between")
-  Iterable<Person> byAgeBetween( //
-      @RequestParam("min") int min, //
-      @RequestParam("max") int max) {
-    return peopleRepository.findByAgeBetween(min, max);
-  }
-
-  @GetMapping("homeloc")
-  Iterable<Person> byHomeLoc(//
-      @RequestParam("lat") double lat, //
-      @RequestParam("lon") double lon, //
-      @RequestParam("d") double distance) {
-    return peopleRepository.findByHomeLoc(new Point(lon, lat), new Distance(distance, Metrics.MILES));
-  }
-
   @GetMapping("name")
   Iterable<Person> byFirstNameAndLastName(@RequestParam("first") String firstName, //
       @RequestParam("last") String lastName) {
     return peopleRepository.findByFirstNameAndLastName(firstName, lastName);
-  }
-
-  @GetMapping("statement")
-  Iterable<Person> byPersonalStatement(@RequestParam("q") String q) {
-    return peopleRepository.searchByPersonalStatement(q);
   }
 
   @PostMapping("new")
@@ -82,27 +58,6 @@ public class PeopleControllerV1 {
   @DeleteMapping("{id}")
   void delete(@PathVariable String id) {
     peopleRepository.deleteById(id);
-  }
-  
-  @GetMapping("city")
-  Iterable<Person> byCity(@RequestParam("city") String city) {
-    return peopleRepository.findByAddress_City(city);
-  }
-  
-  @GetMapping("city_state")
-  Iterable<Person> byCityAndState(@RequestParam("city") String city, //
-      @RequestParam("state") String state) {
-    return peopleRepository.findByAddress_CityAndAddress_State(city, state);
-  }
-  
-  @GetMapping("skills")
-  Iterable<Person> byAnySkills(@RequestParam("skills") Set<String> skills) {
-    return peopleRepository.findBySkills(skills);
-  }
-  
-  @GetMapping("skills/all")
-  Iterable<Person> byAllSkills(@RequestParam("skills") Set<String> skills) {
-    return peopleRepository.findBySkillsContainingAll(skills);
   }
   
   @GetMapping("search/{q}")
